e4b46e1642b7a381e2f84b5a8df9092f5d0f80ce
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-12-12 07:18:47 +0200

    Added minor comments to app.cpp.

--- App.cpp
+++ App.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include "view_source.h"
 
+using namespace Windows::ApplicationModel::Core;
+using namespace xbox;
+
+// ============================================================================
+// The entry point of the application.
+//
+// Note that this function is annotated with [Platform::MTAThread] attribute.
+// This puts application to use multithreaded apartment model for COM interop.
+// ============================================================================
 [Platform::MTAThread]
 int main(Platform::Array<Platform::String^>^)
 {
-	std::cout << "hello xbox!" << std::endl;
+	auto viewSource = ref new ViewSource();
+	CoreApplication::Run(viewSource);
 	return 0;
 }
