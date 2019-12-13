e453f0500a22169db861bfc9356bc935a9686a0c
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-12-13 07:36:51 +0200

    Added listeners for core application, window and display events.

--- view.cpp
+++ view.cpp
@@ -1,18 +1,36 @@
 #include "view.h"
 
 using namespace xbox;
-using namespace Windows::ApplicationModel::Core;
 using namespace Platform;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
 using namespace Windows::UI::Core;
 
 void View::Initialize(CoreApplicationView^ applicationView)
 {
-	// TODO register application event handlers ...
+	// observe the activation of the main view of the application.
+	applicationView->Activated += ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &View::OnActivated);
+	
+	// observe the suspension and resume of the application.
+	CoreApplication::Suspending += ref new EventHandler<SuspendingEventArgs^>(this, &View::OnSuspending);
+	CoreApplication::Resuming += ref new EventHandler<Platform::Object^>(this, &View::OnResuming);
 }
 
 void View::SetWindow(CoreWindow^ window)
 {
-	// TODO register window event handlers ...
+	// observe the events of the main window of the application.
+	window->SizeChanged += ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &View::OnSizeChanged);
+	window->VisibilityChanged += ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &View::OnVisibilityChanged);
+	window->Closed += ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &View::OnClosed);
+
+	// observe changes in the current display.
+	DisplayInformation^ displayInformation = DisplayInformation::GetForCurrentView();
+	displayInformation->DpiChanged += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &View::OnDpiChanged);
+	displayInformation->OrientationChanged += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &View::OnOrientationChanged);
+	displayInformation->DisplayContentsInvalidated += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &View::OnDisplayContentsInvalidated);
 }
 
 void View::Load(String^ entryPoint)
@@ -28,4 +46,51 @@ void View::Run()
 void View::Uninitialize()
 {
 	// TODO perform foreground exit logic ...
+}
+
+// ============================================================================
+
+void View::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+
+}
+
+void View::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+
+}
+
+void View::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+
+}
+
+void View::OnSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args)
+{
+
+}
+
+void View::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+
+}
+
+void View::OnClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+
+}
+
+void View::OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args)
+{
+
+}
+
+void View::OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args)
+{
+
+}
+
+void View::OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args)
+{
+
 }
--- view.h
+++ view.h
@@ -10,5 +10,20 @@ namespace xbox
 		virtual void Load(Platform::String^ entryPoint);
 		virtual void Run();
 		virtual void Uninitialize();
+	protected:
+		// application lifecycle event handlers
+		void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ args);
+		void OnResuming(Platform::Object^ sender, Platform::Object^ args);
+
+		// window event handlers
+		void OnSizeChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::WindowSizeChangedEventArgs^ args);
+		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+		void OnClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
+
+		// display information event handlers
+		void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
 	};
 }
